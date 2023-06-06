c41a775b88a8936322149a46e9f20b06fe0e7392
Cristian Garcia <107603821+CristianGarci42022@users.noreply.github.com>
2023-06-06 18:32:06 -0500

    Interfas de Inicio

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-533-1686094084.js
@@ -0,0 +1,101 @@
+document.addEventListener("DOMContentLoaded", function(){ var ssLinks = document.querySelectorAll( '.uagb-block-fe2f9c12' );
+for ( var j = 0; j < ssLinks.length; j++ ) {
+	var ssLink = ssLinks[j].querySelectorAll( ".uagb-ss__link" );
+	for ( var i = 0; i < ssLink.length; i++ ) {
+		ssLink[i].addEventListener( "click", function() {
+			var social_url = this.dataset.href;
+			var target = "";
+			if( social_url == "mailto:?body=" ) {
+				target = "_self";
+			}
+			var  request_url ="";
+			if( social_url.indexOf("/pin/create/link/?url=") !== -1) {
+				request_url = social_url + encodeURIComponent( window.location.href ) + "&media=" + '';
+			}else{
+				request_url = social_url + encodeURIComponent( window.location.href );
+			}
+			window.open( request_url, target );
+		});
+	}
+}
+var ssLinks = document.querySelectorAll( '.uagb-block-c85182f3' );
+for ( var j = 0; j < ssLinks.length; j++ ) {
+	var ssLink = ssLinks[j].querySelectorAll( ".uagb-ss__link" );
+	for ( var i = 0; i < ssLink.length; i++ ) {
+		ssLink[i].addEventListener( "click", function() {
+			var social_url = this.dataset.href;
+			var target = "";
+			if( social_url == "mailto:?body=" ) {
+				target = "_self";
+			}
+			var  request_url ="";
+			if( social_url.indexOf("/pin/create/link/?url=") !== -1) {
+				request_url = social_url + encodeURIComponent( window.location.href ) + "&media=" + '';
+			}else{
+				request_url = social_url + encodeURIComponent( window.location.href );
+			}
+			window.open( request_url, target );
+		});
+	}
+}
+var ssLinks = document.querySelectorAll( '.uagb-block-37198d09' );
+for ( var j = 0; j < ssLinks.length; j++ ) {
+	var ssLink = ssLinks[j].querySelectorAll( ".uagb-ss__link" );
+	for ( var i = 0; i < ssLink.length; i++ ) {
+		ssLink[i].addEventListener( "click", function() {
+			var social_url = this.dataset.href;
+			var target = "";
+			if( social_url == "mailto:?body=" ) {
+				target = "_self";
+			}
+			var  request_url ="";
+			if( social_url.indexOf("/pin/create/link/?url=") !== -1) {
+				request_url = social_url + encodeURIComponent( window.location.href ) + "&media=" + '';
+			}else{
+				request_url = social_url + encodeURIComponent( window.location.href );
+			}
+			window.open( request_url, target );
+		});
+	}
+}
+var ssLinks = document.querySelectorAll( '.uagb-block-79476659' );
+for ( var j = 0; j < ssLinks.length; j++ ) {
+	var ssLink = ssLinks[j].querySelectorAll( ".uagb-ss__link" );
+	for ( var i = 0; i < ssLink.length; i++ ) {
+		ssLink[i].addEventListener( "click", function() {
+			var social_url = this.dataset.href;
+			var target = "";
+			if( social_url == "mailto:?body=" ) {
+				target = "_self";
+			}
+			var  request_url ="";
+			if( social_url.indexOf("/pin/create/link/?url=") !== -1) {
+				request_url = social_url + encodeURIComponent( window.location.href ) + "&media=" + '';
+			}else{
+				request_url = social_url + encodeURIComponent( window.location.href );
+			}
+			window.open( request_url, target );
+		});
+	}
+}
+var ssLinks = document.querySelectorAll( '.uagb-block-7267e6e4' );
+for ( var j = 0; j < ssLinks.length; j++ ) {
+	var ssLink = ssLinks[j].querySelectorAll( ".uagb-ss__link" );
+	for ( var i = 0; i < ssLink.length; i++ ) {
+		ssLink[i].addEventListener( "click", function() {
+			var social_url = this.dataset.href;
+			var target = "";
+			if( social_url == "mailto:?body=" ) {
+				target = "_self";
+			}
+			var  request_url ="";
+			if( social_url.indexOf("/pin/create/link/?url=") !== -1) {
+				request_url = social_url + encodeURIComponent( window.location.href ) + "&media=" + '';
+			}else{
+				request_url = social_url + encodeURIComponent( window.location.href );
+			}
+			window.open( request_url, target );
+		});
+	}
+}
+ });
